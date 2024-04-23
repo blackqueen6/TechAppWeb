cdbb4752b500d115c04e548bb7a3cd7bc953c743
Klaudia <jula45810@gmail.com>
2024-04-23 21:31:52 +0200

    lab7

--- api/lib/index.ts
+++ api/lib/index.ts
@@ -1,6 +1,9 @@
 import App from "./app";
 import IndexController from "./controllers/index.controller";
 import DataController from "./controllers/data.controller";
+import DataService from "./modules/services/data.service";
 
-const app: App = new App([new IndexController(), new DataController()]);
+const dataService = new DataService();
+const dataController = new DataController(dataService);
+const app: App = new App([new IndexController(), dataController]);
 app.listen();
